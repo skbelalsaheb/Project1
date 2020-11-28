b1ddd6f44f47bb6d2cec4f31b4d190aced1d5939
Sk Belal Saheb <skbelalsaheb@gmail.com>
2020-11-29 00:21:21 +0530

    CRUD Complete & Cover Basic Of JPA REPO

--- src/main/java/com/project1/demo/Service/BooksRepo.java
+++ src/main/java/com/project1/demo/Service/BooksRepo.java
@@ -3,5 +3,8 @@ package com.project1.demo.Service;
 import com.project1.demo.Models.Books;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BooksRepo extends JpaRepository<Books,Integer> {
+    public List<Books> findByBookNameContains(String name);
 }
--- src/main/java/com/project1/demo/Service/FEature.java
+++
@@ -1,4 +0,0 @@
-package com.project1.demo.Service;
-
-public class FEature {
-}
