8b7d3b71940cb0abebeb64bb05dfe0a7ba80a26a
Subhasish Aditya <subhasishadit@gmail.com>
2020-11-29 00:46:34 +0530

    fg

--- src/main/java/com/project1/demo/Service/StudentRepo.java
+++ src/main/java/com/project1/demo/Service/StudentRepo.java
@@ -5,5 +5,5 @@ import com.project1.demo.Models.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StudentRepo extends JpaRepository<Student,Integer>
-{
+{jkfbgk
 }
